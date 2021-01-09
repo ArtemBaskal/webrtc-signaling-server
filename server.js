e1a179deb0cdda6248c2b5c7f7c64193d60a9f8f
Artem Baskal <a.baskal@adguard.com>
2021-01-09 21:59:40 +0300

    Add rooms support

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 require('dotenv').config();
 const fs = require('fs');
 const WebSocket = require('ws');
+const url = require('url');
 
 let http;
 let options;
 
-if (process.env.PROD) {
+if (process.env.BUILD_ENV === 'prod') {
     http = require('http');
-
     options = {};
 } else {
     http = require('https');
-
     options = {
         key: fs.readFileSync('key.pem'),
         cert: fs.readFileSync('cert.pem')
@@ -29,12 +28,45 @@ const server = http.createServer(
 
 const wss = new WebSocket.Server({server});
 
-wss.on("connection", (ws) => {
-    ws.on("open", () => console.log("Opened"));
-    ws.on("close", () => console.log("Closed"));
+/**
+ * @type {{[number] : WebSocket[]}}
+ */
+const ROOMS_TO_CLIENTS_MAP = {};
+const QUERY_PARAM_ROOM_NAME = 'room';
+
+/**
+ *
+ * @param request {object}
+ * @param paramName {string}
+ * @returns {string}
+ */
+const getQueryParam = (request, paramName) => {
+    const query = url.parse(request.url).query;
+    const urlSearchParams = new URLSearchParams(query);
+
+    return urlSearchParams.get(paramName);
+};
+
+/* TODO add ws bufferization? */
+wss.on("connection", (ws, request) => {
+    const room = getQueryParam(request, QUERY_PARAM_ROOM_NAME);
+    console.log('room=%s', room);
+    const clientsInRoom = ROOMS_TO_CLIENTS_MAP[room] || [];
+    ROOMS_TO_CLIENTS_MAP[room] = clientsInRoom.concat(ws);
+
+    ws.on("close", (code, reason) => {
+        console.log("Closed: code %d, reason %s", code, reason);
+        ROOMS_TO_CLIENTS_MAP[room] = ROOMS_TO_CLIENTS_MAP[room].filter((client) => client !== ws);
+
+        if (ROOMS_TO_CLIENTS_MAP[room].length === 0) {
+            delete ROOMS_TO_CLIENTS_MAP[room];
+        }
+    });
+
     ws.on("message", (message) => {
-        wss.clients.forEach((client) => {
-            // A client WebSocket broadcasting to all connected WebSocket clients, excluding itself
+        console.log('message=%s', room);
+        ROOMS_TO_CLIENTS_MAP[room].forEach((client) => {
+            // A client WebSocket broadcasting to all connected WebSocket clients in this room, excluding itself
             if (client !== ws && client.readyState === WebSocket.OPEN) {
                 client.send(message)
             }
