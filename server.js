760fee8877daed53574eb31082a078613b1ff895
Artem Baskal <artem.baskal.dev@gmail.com>
2021-01-19 01:05:02 +0300

    Enhance logging

--- server.js
+++ server.js
@@ -59,25 +59,26 @@ const getQueryParam = (request, paramName) => {
 /* TODO add ws bufferization? */
 wss.on("connection", (ws, request) => {
     const room = getQueryParam(request, QUERY_PARAM_ROOM_NAME);
-    console.log('room=%s', room);
+    console.log("Connect to room: '%s'.", room);
     const clientsInRoom = ROOMS_TO_CLIENTS_MAP[room] || [];
     ROOMS_TO_CLIENTS_MAP[room] = clientsInRoom.concat(ws);
 
     ws.on("close", (code, reason) => {
-        console.log("Closed: code %d, reason %s", code, reason);
         ROOMS_TO_CLIENTS_MAP[room] = ROOMS_TO_CLIENTS_MAP[room].filter((client) => client !== ws);
+        console.log("Close connection: code '%d', reason '%s'.", code, reason);
 
         if (ROOMS_TO_CLIENTS_MAP[room].length === 0) {
             delete ROOMS_TO_CLIENTS_MAP[room];
+            console.log("Delete room: '%s'.", room);
         }
     });
 
     ws.on("message", (message) => {
-        console.log('message=%s', room);
         ROOMS_TO_CLIENTS_MAP[room].forEach((client) => {
             // A client WebSocket broadcasting to all connected WebSocket clients in this room, excluding itself
             if (client !== ws && client.readyState === WebSocket.OPEN) {
-                client.send(message)
+                client.send(message);
+                console.log("Send message: '%s'", message);
             }
         })
     });
@@ -85,5 +86,5 @@ wss.on("connection", (ws, request) => {
 
 const PORT = process.env.PORT || 8000;
 
-server.listen(PORT, () => console.log('Listening on port %s', PORT));
+server.listen(PORT, () => console.log("Listening on port %s", PORT));
 
